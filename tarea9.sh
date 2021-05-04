efb5903e9458617e3def0911c3428e2189f2fc69
Mpazlaboratorio <73123654+Mpazlaboratorio@users.noreply.github.com>
2021-05-04 20:48:16 +0200

    cambios directorio

--- tarea9.sh
+++ tarea9.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+import os
+os.environ("HOME")
+DIRECTORIO=os.path.expanduser("~/")
+
 DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
 
 echo "Hoy es el $DIA y la hora actual es $HORA!"
+echo "$DIRECTORIO es un directorio y su tamaño es el siguiente:"
+du -hs $DIRECTORIO
 
