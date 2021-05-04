5042174d563ed3f490e4b2a3e6cc6cab9c34fb55
Mpazlaboratorio <73123654+Mpazlaboratorio@users.noreply.github.com>
2021-05-04 20:17:55 +0200

    servicios

--- tarea8.sh
+++ tarea8.sh
@@ -1,2 +1,18 @@
-#!/bin/bash
-ps axo pid,ppid,pgrp,tty,tpgid,sess,comm |awk '$2==1' |awk '$1==$3'
+#!/bin/sh
+# ---------------------------------------------
+# Checha estatus de servicios activos
+# Sintaxis: sh servicio.sh {nombre_del_servicio}
+# Ejemplo:  sh servicio.sh httpd
+# ----------------------------------------------
+ 
+CHECK=$0
+SERVICE=$1
+DATE=`date`
+OUTPUT=$(ps aux | grep -v grep | grep -v $CHECK |grep $1)
+#
+if [ "${#OUTPUT}" -gt 0 ] ; then
+ echo "$DATE: $SERVICE : Servicio ACTIVO, todo marcha bien."
+else
+ echo "$DATE: $SERVICE : INACTIVO o NO Existente, verificar el problema."
+fi
+exit 0
